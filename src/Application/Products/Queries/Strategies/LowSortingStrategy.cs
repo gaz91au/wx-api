a4f51bd6ce861b04a19c1ac4c7c96531b9c6773f
gaz91au <gary.huynh91@gmail.com>
2020-10-26 02:25:30 +1100

    remove unused using statements

--- src/Application/Products/Queries/Strategies/LowSortingStrategy.cs
+++ src/Application/Products/Queries/Strategies/LowSortingStrategy.cs
@@ -1,5 +1,4 @@
-﻿using Application.Common.Exceptions;
-using Application.Common.Interfaces;
+﻿using Application.Common.Interfaces;
 using Domain.Entities;
 using System.Collections.Generic;
 using System.Linq;
