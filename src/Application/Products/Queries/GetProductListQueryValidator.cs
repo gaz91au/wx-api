e944bc17b41b252e97d72e62764257ebf5fa18df
gaz91au <gary.huynh91@gmail.com>
2020-10-26 01:41:44 +1100

    add validation

---
+++ src/Application/Products/Queries/GetProductListQueryValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Application.Products.Queries
+{
+    public class GetProductListQueryValidator : AbstractValidator<GetProductListQuery>
+    {
+        public GetProductListQueryValidator()
+        {
+            RuleFor(x => x.SortOption)
+                .NotNull().WithMessage("SortOption must not be null.")
+                .NotEmpty().WithMessage("SortOption must not be empty.");
+        }
+    }
+}
