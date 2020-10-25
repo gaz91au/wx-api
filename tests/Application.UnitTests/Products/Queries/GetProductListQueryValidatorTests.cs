bf24223168e1f7e7f830b2fd7567427fe3f4ebf6
gaz91au <gary.huynh91@gmail.com>
2020-10-26 01:44:08 +1100

    add validator unit tests

---
+++ tests/Application.UnitTests/Products/Queries/GetProductListQueryValidatorTests.cs
@@ -0,0 +1,22 @@
+﻿using Application.Products.Queries;
+using FluentValidation.TestHelper;
+using Xunit;
+
+namespace Application.UnitTests.Products.Queries
+{
+    public class GetProductListQueryValidatorTests
+    {
+        private readonly GetProductListQueryValidator _validator;
+
+        public GetProductListQueryValidatorTests()
+        {
+            _validator = new GetProductListQueryValidator();
+        }
+
+        [Fact]
+        public void GivenEmptySortOption_ShouldHaveValidationError()
+        {
+            _validator.ShouldHaveValidationErrorFor(x => x.SortOption, string.Empty);
+        }
+    }
+}
